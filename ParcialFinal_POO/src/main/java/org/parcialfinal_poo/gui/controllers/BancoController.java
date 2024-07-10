e937473e3735918ffc8e181d6e2ff06c84a8a6dc
Mauricio Montes <00042823@uca.edu.sv>
2024-07-09 21:28:05 -0600

    :children_crossing: Validación de datos para reportes terminada

--- ParcialFinal_POO/src/main/java/org/parcialfinal_poo/gui/controllers/BancoController.java
+++ ParcialFinal_POO/src/main/java/org/parcialfinal_poo/gui/controllers/BancoController.java
@@ -1,7 +1,6 @@
 package org.parcialfinal_poo.gui.controllers;
 
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import org.parcialfinal_poo.models.Banco.Cliente;
@@ -17,7 +16,6 @@ import java.util.ArrayList;
 
 import static java.sql.DriverManager.getConnection;
 
-import org.parcialfinal_poo.models.DataBase.QueriesReportes.Queries;
 import org.parcialfinal_poo.models.DataBase.Selects.Select;
 import org.parcialfinal_poo.models.TextFiles.TextFiles;
 
@@ -117,16 +115,29 @@ public class BancoController {
     private ListView<String> lvCompraDescripcion = new ListView<>(); //00042823 Crea un ListView para la descripción de la compra (debatí si la tabla debería mostrar esto, legítimamente)
 
     @FXML
-    private Label campoObligatorio1, campoObligatorio2, campoObligatorio3;
+    private Label
+            errorLabel1,
+            errorLabel2,
+            errorLabel3,
+            errorLabel4, //00042823 Label para el reporte B que indicará al cliente algún error en el ingreso de datos
+            errorLabel5, //00042823 Otro label, para otro campo, siempre del reporte B
+            errorLabel6, //00042823 Y un último label para la validación de datos para el reporte B
+            errorLabel7, //00042823 Label para validar datos para el reporte C
+            errorLabel8; //00042823 Label para validar datos para el reporte D
 
 
     @FXML
     // 00022423 Método que se ejecuta cuando la interfaz gráfica se inicializa
     public void initialize() {
 
-        campoObligatorio1.setVisible(false);
-        campoObligatorio2.setVisible(false);
-        campoObligatorio3.setVisible(false);
+        errorLabel1.setVisible(false);
+        errorLabel2.setVisible(false);
+        errorLabel3.setVisible(false);
+        errorLabel4.setVisible(false);
+        errorLabel5.setVisible(false);
+        errorLabel6.setVisible(false);
+        errorLabel7.setVisible(false);
+        errorLabel8.setVisible(false);
 
         cmbFacilitador.getItems().addAll(Facilitador.Visa, Facilitador.MasterCard, Facilitador.AmericanExpress); //00042823 Se meten las opciones para el ComboBox de facilitadores para generar el reporte D
 
@@ -141,24 +152,38 @@ public class BancoController {
     }
 
     private void mostrarReporteA() {
-        campoObligatorio1.setVisible(false);
-        campoObligatorio2.setVisible(false);
-        campoObligatorio3.setVisible(false);
+        errorLabel1.setVisible(false);
+        errorLabel2.setVisible(false);
+        errorLabel3.setVisible(false);
 
-        boolean flag = false;
 
-        if (tfIdClienteRA.getText().isEmpty()) {
-            campoObligatorio1.setVisible(true);
-            flag = true;
+        boolean flag = false; //TODO: Comentar (Gabo)
+
+        try{ //00042823 Nueva validación, que el ID sea un entero
+            Integer.parseInt(tfIdClienteRA.getText()); //00042823 Hace la transformación solo para ver si lanza una excepción
+        }
+        catch (NumberFormatException e){ //00042823 Si se lanza la excepción, entonces...
+
+
+            if (tfIdClienteRA.getText().isEmpty()) {
+                errorLabel1.setText("Campo obligatorio");
+            }
+            else {
+                errorLabel1.setText("Ingrese un ID válido"); //00042823 Se cambia el label para hacerle ver el problema al usuario
+            }
+            errorLabel1.setVisible(true); //00042823 El label, invisible por defecto, se hace visible para resolver el error de capa 8
+            flag = true; //00042823 Se levanta la bandera
         }
 
         if (dpFechaInicial.getValue() == null) {
-            campoObligatorio2.setVisible(true);
+            errorLabel2.setText("Campo obligatorio");
+            errorLabel2.setVisible(true);
             flag = true;
         }
 
         if (dpFechaFinal.getValue() == null) {
-            campoObligatorio3.setVisible(true);
+            errorLabel3.setText("Campo obligatorio");
+            errorLabel3.setVisible(true);
             flag = true;
         }
 
@@ -174,90 +199,158 @@ public class BancoController {
                         compra.getMonto() + "\n" + compra.getTarjetaID() + "\n\n");
             }
 
-            taMuestraReporte.setText(text);
-            TextFiles.createFile('A',text); //TODO: comentar
+            if (!text.isEmpty()) { //00042823 Esto solo debería ocurrir si el ID del cliente no existe, que daría como resultado un ResultSet vacío y, por tanto, nada que reportar
+                taMuestraReporte.setText(text);
+                TextFiles.createFile('A', text); //TODO: Comentar (Gabo)
+            } else { //00042823 Si, en efecto, no hay nada que reportar, pues entonces no se crea el reporte (¿para qué tener un .txt vacío?)
+                taMuestraReporte.setText("Nada que reportar..."); //00042823 Muestra en el TextArea que no hay nada que reportar
+            }
         }
     }
 
     private void mostrarReporteB() {
-        try {
+        errorLabel4.setVisible(false); //00042823 Por defecto...
+        errorLabel5.setVisible(false); //00042823 Los labels de error...
+        errorLabel6.setVisible(false); //00042823 Son invisibles
+
+        // 00022423 Definir el rango de años válidos
+        int anioMinimo = 1900;
+        int anioMaximo = 2100;
+        String mes = cbMes.getValue();
+
+        boolean flag = false; //00042823 Se crea una bandera para validar datos
+
+        try { //00042823 Se verifica que el año ingresado sea un número para empezar
+            Integer.parseInt(tfIdClienteRB.getText()); //00042823 Si no es número, lanza una excepción
+        } catch (NumberFormatException e) { //00042823 Se atrapa la excepción, si ocurre
+            if (tfIdClienteRB.getText().isEmpty()) { //00042823 Se verifica si el usuario llenó el campo
+                errorLabel4.setText("Campo obligatorio"); //00042823 El usuario no llenó el campo
+                errorLabel4.setVisible(true); //00042823 Se hace visible el label
+                flag = true; //00042823 Se levanta la bandera
+            }
+            else {
+                errorLabel4.setText("Ingrese un año válido"); //00042823 Establece el tipo de error
+                errorLabel4.setVisible(true); //00042823 Y se lo muestra al usuario
+                flag = true; //00042823 Se levanta la bandera
+            }
+        }
+
+        if (mes == null) {//00042823 La única forma de que mes sea null es que no ha seleccionada nada
+            errorLabel5.setText("Campo obligatorio"); //00042823 Se le olvidó llenar este campo también
+            errorLabel5.setVisible(true); //00042823 Se hace visible el label
+            flag = true; //00042823 Se levanta la bandera
+        }
+
+        try { //00042823 Se verifica que el año ingresado sea un número para empezar
+            int anio = Integer.parseInt(tfAnio.getText()); //00042823 Si no es número, lanza una excepción
+
+            if (anio >= anioMaximo || anio < anioMinimo) { //00042823 Verifica si el año ingresado se encuentra dentro del rango establecido arbitrariamente
+                errorLabel6.setText("Año fuera de rango (1900 - 2100)"); //00042823 Es medianamente realista, además de permitir evitar números negativos
+                errorLabel6.setVisible(true); //00042823 Se hace visible el label
+                flag = true; //00042823 Se levanta la bandera
+            }
+        } catch (NumberFormatException e) { //00042823 Se atrapa la excepción, si ocurre
+            if (tfAnio.getText().isEmpty()) { //00042823 Si el campo está vacío
+                errorLabel6.setText("Campo obligatorio"); //00042823 Y también se le olvidó llenar este campo
+            }
+            else { //00042823 Entonces, el usuario ingreso algo que no es un número
+                errorLabel6.setText("Ingrese un año válido"); //00042823 Establece el tipo de error
+            }
+            errorLabel6.setVisible(true); //00042823 Se hace visible el label
+            flag = true;//00042823 Se levanta la bandera
+        }
+
+        if (!flag) { //00042823 Si la bandera NO está levantada
+
             // 00022423 Obtener los valores ingresados por el usuario
             int clienteID = Integer.parseInt(tfIdClienteRB.getText());
-            String mes = cbMes.getValue();
             int anio = Integer.parseInt(tfAnio.getText());
 
-            // 00022423 Definir el rango de años válidos
-            int anioMinimo = 1900;
-            int anioMaximo = 2100;
+            // 00022423 Convertir el mes en índice (1-12)
+            int mesIndex = cbMes.getItems().indexOf(mes) + 1;
+            // 00022423 Obtener el total de gasto del cliente para el mes y año seleccionados
+            double totalGasto = Queries.getInstance().generarReporteB(clienteID, anio, mesIndex);
+            // 00022424 Mostrar el resultado en el TextArea
 
-            // 00022423 Verificar que se hayan seleccionado mes y año válidos
-            if (mes != null && !mes.isEmpty() && anio >= anioMinimo && anio <= anioMaximo) {
-                // 00022423 Convertir el mes en índice (1-12)
-                int mesIndex = cbMes.getItems().indexOf(mes) + 1;
-                // 00022423 Obtener el total de gasto del cliente para el mes y año seleccionados
-                double totalGasto = Queries.getInstance().generarReporteB(clienteID, anio, mesIndex);
-                // 00022424 Mostrar el resultado en el TextArea
+            String text = "El gasto total del Cliente ID " + clienteID + " en " + mes.toLowerCase() + " " + anio + " es: $" + totalGasto;
 
-                String text = "El gasto total del Cliente ID " + clienteID + " en " + mes.toLowerCase() + " " + anio + " es: $" + totalGasto;
-
+            if (totalGasto != -1) { //00042823 Si el gasto es -1, entonces el cliente no existe
                 taMuestraReporte.setText(text);
-                TextFiles.createFile('B',text); //TODO: comentar
-            } else {
-                // 00022423 Mostrar una alerta si los datos no son válidos
-                mostrarAlerta("Entrada Inválida", "Por favor, seleccione un mes y un año válidos entre " + anioMinimo + " y " + anioMaximo + ".");
+                TextFiles.createFile('A', text); //TODO: Comentar (Rocía)
+            } else { //00042823 Si, en efecto, no hay nada que reportar, pues entonces no se crea el reporte (¿para qué tener un .txt vacío?)
+                taMuestraReporte.setText("Nada que reportar..."); //00042823 Muestra en el TextArea que no hay nada que reportar
             }
-
-        } catch (NumberFormatException e) {
-            // 00022423 Mostrar una alerta si los datos no son válidos (formato de número)
-            mostrarAlerta("Entrada Inválida", "Por favor, ingrese un ID de cliente y un año válidos.");
-        } catch (Exception e) {
-            // 00022423 Mostrar una alerta si ocurre un error al generar el reporte
-            mostrarAlerta("Error", "Ocurrió un error al generar el reporte: " + e.getMessage());
         }
     }
 
     private void mostrarReporteC() {//00088023 Método encargado de realizar el reporte C
-        try {
+        errorLabel7.setVisible(false);
+
+        boolean flag = false; //00042823 Se crea una bandera para validar datos
+
+        try { //00042823 Verifica si el TextField tiene un número entero
+            Integer.parseInt(tfIdClienteRC.getText()); //00042823 Lanza una excepción si el número no es entero
+        } catch (NumberFormatException e) { //00042823 En caso de que se haya lanzado una excepción...
+            if (tfIdClienteRC.getText().isEmpty()) { //00042823 Se verifica si el usuario llenó el campo
+                errorLabel4.setText("Campo obligatorio"); //00042823 El usuario no llenó el campo
+            } else { //00042823 Entonces, el usuario ingreso algo que no es un número
+                errorLabel4.setText("Ingrese un año válido"); //00042823 Establece el tipo de error
+            }
+            errorLabel4.setVisible(true); //00042823 Se hace visible el label
+            flag = true; //00042823 Se levanta la bandera
+        }
+
+        if (!flag) { //00042823 Si la bandera no está levantada...
+            String text = "";
             taMuestraReporte.setText("");//00088023 Primeramente se limpia el textArea
             int ClienteID = Integer.parseInt(tfIdClienteRC.getText());//00088023 Se Recibe el id del cliente del texArea y lo convierte en entero
             ArrayList<String> credito = new ArrayList<>(), debito = new ArrayList<>();//00088023 Inicializa dos arrays, uno que guarda las tarjetas de crédito, y el otro de débito
             Queries.getInstance().generarReporteC(ClienteID, credito, debito);//00088023 Llama al generaador de reporte que llene los arrays
 
-            String text = "Tarjetas de crédito del cliente:\n"; //00088023 Empieza a darle formato al texto a mostrar
-            if (credito.isEmpty()) {//00088023 Revisa si el array está vacio
-                text = text.concat("N/A");//00088023 Si está vacio entonces le pone como texto "N/A"
-            } else {
-                for (String tarjeta : credito) { //00088023 Si no está vacio entonces empieza a ingresar todas las tarjetas de crédito
-                    text = text.concat(tarjeta + "\n");
+            for (Cliente c : Select.getInstance().selectCliente()) { //00042823 Llamamos a la base de datos para verificar si, en efecto, existe el cliente
+                if (ClienteID == c.getId()) { //00042823 Verifica si clienteID coincide con el ID de algún cliente,
+
+                    text = "Tarjetas de crédito del cliente:\n"; //00088023 Empieza a darle formato al texto a mostrar
+                    if (credito.isEmpty()) {//00088023 Revisa si el array está vacio
+                        text = text.concat("N/A");//00088023 Si está vacio entonces le pone como texto "N/A"
+                    } else {
+                        for (String tarjeta : credito) { //00088023 Si no está vacio entonces empieza a ingresar todas las tarjetas de crédito
+                            text = text.concat(tarjeta + "\n");
+                        }
+                    }
+
+                    text = text.concat("\n\nTarjetas de débito del cliente:\n");//00088023 Le da formato al texto para mostrar las tarjetas de débito
+
+                    if (debito.isEmpty()) {//00088023 Revisa si el array no está vacio
+                        text = text.concat("N/A");//00088023 Si lo está, entonces escribe "N/A"
+                    } else {
+                        for (String tarjeta : debito) {//00088023 Si no esta vacio, entonces escribe todas las tarjetas en el texto
+                            text = text.concat(tarjeta + "\n");
+                        }
+                    }
+
+                    taMuestraReporte.setText(text);//00088023 Muestra el texto en el TextArea
+                    TextFiles.createFile('C', text); //TODO: comentar
+
+                    break; //00042823 Ya no continuará recorriendo la lista de IDs
+
+                } else { //Si no encuentra una coincidencia, entonces no hay nada que reportar
+                    taMuestraReporte.setText("Nada que reportar..."); //00042823 Muestra en el TextArea que no hay nada que reportar
                 }
             }
-
-            text = text.concat("\n\nTarjetas de débito del cliente:\n");//00088023 Le da formato al texto para mostrar las tarjetas de débito
-
-            if (debito.isEmpty()) {//00088023 Revisa si el array no está vacio
-                text = text.concat("N/A");//00088023 Si lo está, entonces escribe "N/A"
-            } else {
-                for (String tarjeta : debito) {//00088023 Si no esta vacio, entonces escribe todas las tarjetas en el texto
-                    text = text.concat(tarjeta + "\n");
-                }
-            }
-
-            taMuestraReporte.setText(text);//00088023 Muestra el texto en el TextArea
-            TextFiles.createFile('C',text); //TODO: comentar
-
-        } catch (Exception e) {
-            mostrarAlerta("Error", "Ingrese un valor válido");//00088023 Este error se recibe por el parseInteger, por lo tanto el usuario ingreso un valor no válido
         }
     }
 
     private void mostrarReporteD() {
+        errorLabel8.setVisible(false);
+
         if (cmbFacilitador.getValue() != null) { //00042823 Si hay una opción selecciona por cmbFacilitador...
             String text = ""; //00042823 Es una cadena vacía que tiene como propósito concatenar el contenido completo del reporte D
             ResultSet rs = Queries.getInstance().generarReporteD(cmbFacilitador.getValue()); //00042823 Se llama al singleton Queries para hacer la consulta para el reporte D con el facilitador seleccionado, el cual se almacena en un ResultSet
 
-            boolean flag = true; //00042823 Se crea una bandera que sirva para indicar si hay valores en el ResultSet
+            boolean flag = false; //00042823 Se crea una bandera que sirva para indicar si hay valores en el ResultSet
             try { //00042823 El método next() para ResultSet lanza SQLExceptions que no pueden dejarse irresolubles
-                while (flag) { //00042823 Bucle para recorrer las "filas" de la tabla a consultar
+                while (!flag) { //00042823 Bucle para recorrer las "filas" de la tabla a consultar
                     if (rs.next()) {
                         text = text.concat( //00042823 Por cada cliente, se concatena la siguiente información
                                 "Cliente: " + rs.getInt("id") + "\n" //00042823 El ID del cliente
@@ -267,7 +360,7 @@ public class BancoController {
                                         + "Gasto total: $" + rs.getDouble("total") + "\n\n" //00042823 Cuánto ha gastado en total el cliente por medio del facilitador
                         );
                     } else { //00042823 Si ya no hay filas por recorrer en el ResultSet
-                        flag = false; //00042823 Baja la bandera
+                        flag = true; //00042823 Se levanta la bandera
 
                         DataBase dataBase = new DataBase() {}; //00042823 Se instancia la clase abstracta (this is so wrong) para poder tener acceso a la conexión de la base de datos
                         dataBase.getConnection().close(); //00042823 Se cierra la conexión, que quedaba abierta por usar el ResultSet
@@ -281,7 +374,8 @@ public class BancoController {
             }
 
         } else { //00042823 Si no hay nada seleccionado por cmbFacilitador
-            //TODO: Crear una alerta, o bien un label error, ambas son opciones válidas
+            errorLabel8.setText("Campo obligatorio");
+            errorLabel8.setVisible(true);
         }
     }
 
